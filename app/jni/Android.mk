a1ea7428007732619a2d907e40c58138d0135aa9
Craig <craig@oppkey>
2019-04-02 11:09:48 -0700

    added opencv

--- app/jni/Android.mk
+++ app/jni/Android.mk
@@ -4,7 +4,7 @@ include $(CLEAR_VARS)
 
 OPENCV_INSTALL_MODULES:=on
 OPENCV_LIB_TYPE:=SHARED
-include /home/craig/Development/ricoh/plugin/opencv-3.4.5-android-sdk/OpenCV-android-sdk/sdk/native/jni/OpenCV.mk
+include C:\Users\craigdev\Development\no-camera-opencv\OpenCV-android-sdk/sdk/native/jni/OpenCV.mk
 LOCAL_MODULE := opencvsample
 LOCAL_SRC_FILES := sample.cpp
 include $(BUILD_SHARED_LIBRARY)
