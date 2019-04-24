a22e3a43d981c83daa4a97067c9774f454ae8786
Craig <craig@oppkey.com>
2019-04-24 11:38:03 -0700

    updated with opencv4

--- app/jni/Android.mk
+++ app/jni/Android.mk
@@ -4,7 +4,7 @@ include $(CLEAR_VARS)
 
 OPENCV_INSTALL_MODULES:=on
 OPENCV_LIB_TYPE:=SHARED
-include D:\project\opencv-4.0.1-android-sdk\OpenCV-android-sdk\sdk/native/jni/OpenCV.mk
+include /home/craig/Development/ricoh/plugin/contest/no-camera-opencv/OpenCV-android-sdk/sdk/native/jni/OpenCV.mk
 LOCAL_MODULE := opencvsample
 LOCAL_SRC_FILES := sample.cpp
 include $(BUILD_SHARED_LIBRARY)
